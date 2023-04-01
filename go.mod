f569bd19babb7aeb7429d551a9f69ea7083d93ed
feleskatze <no0risk0no0return.03@gmail.com>
2023-04-01 21:53:03 +0900

    2nd

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module mim
 
 go 1.20
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	golang.org/x/sys v0.0.0-20190916202348-b4ddaad3f8a3 // indirect
+)
--- mim.go
+++ mim.go
@@ -1,46 +1,169 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"compress/zlib"
+	"crypto/sha256"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-func main() {
-	Mim_add()
-}
+func MimInit() {
+	inf := []string{".mim", ".mim/refs", ".mim/object"}
 
-func Mim_init() {
-	if d, err := os.Stat(".mim"); os.IsNotExist(err) || !d.IsDir() {
-		//target
-		inf := []string{".mim/index", ".mim/refs", ".mim/object"}
-		for _, v := range inf {
-			if err := os.MkdirAll(v, 0755); err != nil {
+	log.Println("==== INIT Start ====")
+
+	for _, v := range inf {
+		if d, err := os.Stat(v); os.IsNotExist(err) || !d.IsDir() {
+			if err := os.MkdirAll(v, os.ModePerm); err != nil {
 				log.Fatal(err)
 			} else {
-				log.Println("Make: ", v)
+				log.Println("INIT: make ", v)
 			}
+		} else {
+			log.Println("INIT: Has Been Exist ", v)
 		}
-	} else {
-		log.Println("Init Has Been")
 	}
-
+	log.Printf("==== INIT Finish ====\n\n")
 }
 
-func Mim_add() {
-	f, err := os.Open("./testfile.txt")
+func MimAdd() {
+	log.Println("==== Add Start ====")
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() && d.Name() == ".mim" {
+			return filepath.SkipDir
+		}
+		if d.IsDir() && d.Name() == ".git" {
+			return filepath.SkipDir
+		}
+
+		if !d.IsDir() {
+			path, _ := filepath.Rel(dir, path)
+			status, path, hash := makeBlob(path)
+			writeIndex(status, path, hash)
+		}
+		return err
+	})
+
+	log.Printf("==== Add Finish ====\n\n")
+}
+
+func makeBlob(filename string) (string, string, [28]byte) {
+	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	data := make([]byte, 1024)
-	count, err := f.Read(data)
+	//read target file
+	var data []byte
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data = append(data, scanner.Bytes()...)
+	}
+
+	blobContent := bytes.Join([][]byte{[]byte("blob "), []byte("\x00"), data}, []byte(""))
+
+	//compress zlib
+	var com bytes.Buffer
+	compressor, _ := zlib.NewWriterLevel(&com, zlib.BestCompression)
+	compressor.Write(blobContent)
+	compressor.Close()
+
+	//sha224
+	hash := sha256.Sum224(blobContent)
+
+	//open write file
+	blobFile, err := os.Create(fmt.Sprintf(".mim/object/%x", hash))
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer blobFile.Close()
 
-	fmt.Println(count)
-	fmt.Println(string(data[:count]))
+	//make blob file
+	writer := bufio.NewWriter(blobFile)
+	if _, err := writer.Write(com.Bytes()); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Make Blob: %s : %x\n", filename, hash)
+	}
+	writer.Flush()
 
+	status := "create"
+	path := filename
+	return status, path, hash
+
+}
+
+func writeIndex(status string, path string, hash [28]byte) {
+	//Write IndexFile
+	indexFile, err := os.OpenFile(".mim/index", os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer indexFile.Close()
+
+	hash16x := fmt.Sprintf("%x\n", hash)
+	indexContent := bytes.Join([][]byte{[]byte(status), []byte(" "), []byte(path), []byte(" "), []byte(hash16x)}, []byte(""))
+
+	indexWrite := bufio.NewWriter(indexFile)
+	if _, err := indexWrite.Write(indexContent); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Add INDEX: %s : %s\n", path, status)
+	}
+	indexWrite.Flush()
+}
+
+func MimCommit() {
+
+}
+
+func test(filename string, hash string) (status string) {
+
+	var indexData [][]string
+
+	f, err := os.Open(".mim/Index")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fileContent := scanner.Text()
+		dataArr := strings.Split(fileContent, " ")
+		indexData = append(indexData, dataArr)
+	}
+	for i := 0; i < len(indexData); i++ {
+		fmt.Println(indexData[i][1])
+		if filename == indexData[i][1] {
+			if hash == indexData[i][2] {
+				status = "noChange"
+			} else {
+				status = "Change"
+			}
+		} else {
+			status = "create"
+		}
+	}
+	return
+}
+
+func main() {
+	status := test("testfile.txt", "186326af9ecd40de5cb11b6655c00ffb5696677335e0bd23cc888c4b")
+	fmt.Println(status)
+	// MimInit()
+	// MimAdd()
 }
